e23a10fed04f5cbbf76ffc1778f1bc668c5b752f
leonardomessias98 <messiasleonardo98@gmail.com>
2021-04-02 09:24:29 -0300

    implementing login page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
-import Home from 'pages/Home';
+import SignIn from 'pages/SignIn';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={SignIn} />
       </Switch>
     </BrowserRouter>
   );
